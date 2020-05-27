78811b89b2eec9bc6c647149713609c90508cae1
zanderhavgaard <zander@havgaard.dk>
2020-05-27 22:54:26 +0200

    fixed typo in arch install script

--- arch_install/arch_install_utils.sh
+++ arch_install/arch_install_utils.sh
@@ -83,7 +83,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     # setup ufw
     sudo ufw default deny incoming
     sudo ufw default allow outgoing
-    sudo ufw enbale
+    sudo ufw enable
     sudo systemctl enable ufw
 fi
 

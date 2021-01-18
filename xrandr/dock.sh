89c5a2ed7ba50e2e7dc93ad5b3b5bcbcbc59c11d
zanderhavgaard <zander@havgaard.dk>
2021-01-18 10:48:48 +0100

    tweaked rate of dock.sh

--- xrandr/dock.sh
+++ xrandr/dock.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 xrandr \
-    --output DP2-1 --mode 1920x1080 --rate 60 --primary --left-of eDP1 \
-    --output DP2-3 --mode 1920x1080 --rate 60 --left-of DP2-1
+    --output DP2-1 --mode 1920x1080 --rate 30 --primary --left-of eDP1 \
+    --output DP2-3 --mode 1920x1080 --rate 30 --left-of DP2-1

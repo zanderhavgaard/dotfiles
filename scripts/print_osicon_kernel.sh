9f0cb0aac103192f0f0a3e76c1dd71733f4bec43
zanderhavgaard <zander@havgaard.dk>
2021-08-01 15:58:07 +0200

    tweaked kernet print script

--- scripts/print_osicon_kernel.sh
+++ scripts/print_osicon_kernel.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-[[ $(uname -r) =~ ([0-9]+\.[0-9]+\.[0-9]+[\-0-9]*[\-lts]*) ]]
+# TODO fix showing if LTS and arch / manjaro
+[[ $(uname -r) =~ ([0-9]+\.[0-9]+\.[0-9]+) ]]
 
 # echo "  ${BASH_REMATCH[1]}"
 echo "${BASH_REMATCH[1]} "

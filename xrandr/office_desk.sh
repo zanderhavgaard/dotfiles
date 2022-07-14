82e5c20c2ed9dade53f297100f94819491ef865d
zanderhavgaard <zander@havgaard.dk>
2022-07-14 12:10:48 +0200

    updated office desk xrandr script

--- xrandr/office_desk.sh
+++ xrandr/office_desk.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 xrandr \
 	--output eDP1 --off \
-	--output DP2-3 --mode 2560x1440 --rate 30 \
-	--output DP2-1 --mode 1920x1080 --rate 30 --left-of DP2-3
+	--output DP2-3 --primary --auto \
+	--output DP1 --auto --left-of DP2-3

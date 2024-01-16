ebd7513d4faa41ee2c41d1fc9db10fab063103a5
zanderhavgaard <zander@havgaard.dk>
2024-01-16 11:14:23 +0100

    Revert "update office.sh wlr-randr script"
    This reverts commit daa68ac012afb7749292ccb4bab19beab89054a4.

--- wlr-randr/office.sh
+++ wlr-randr/office.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 wlr-randr \
-	--output eDP-2 --pos 2560,1440 --preferred --scale 1.0 \
+	--output eDP-1 --pos 2560,1440 --preferred --scale 1.0 \
 	--output DP-7 --pos 0,0 --preferred --scale 1.0 \
 	--output DP-8 --pos 2560,0 --preferred --scale 1.0
 

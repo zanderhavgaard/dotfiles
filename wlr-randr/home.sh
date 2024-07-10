37d6d9f644890f3a2eafb5f09ed5d34673d8c00b
zanderhavgaard <zander@havgaard.dk>
2024-07-10 12:15:02 +0200

    tweak home.sh wlr-randr script

--- wlr-randr/home.sh
+++ wlr-randr/home.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 wlr-randr --output DP-1 --on --pos 0,0 --preferred --scale 1.0
-wlr-randr --output DP-2 --on --pos 3440,0 --mode 2560x1440@119.880997 --scale 1.0
+wlr-randr --output DP-2 --on --pos 3440,0 --preferred --scale 1.0

af162f4d40de79b6db7a0ca1b2050c876da73404
zanderhavgaard <zander@havgaard.dk>
2024-07-17 13:38:10 +0200

    tweak waybar config

--- scripts/print_osicon_kernel.sh
+++ scripts/print_osicon_kernel.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+OS_NAME="$(rg '^NAME=' /etc/os-release)"
+
+if [[ $OS_NAME == *"Arch"* ]]; then
+	OS_ICON=" "
+elif [[ $OS_NAME == *"Manjaro"* ]]; then
+	OS_ICON=" "
+fi
+
 # TODO fix showing if LTS and arch / manjaro
 [[ $(uname -r) =~ ([0-9]+\.[0-9]+\.[0-9]+) ]]
 
-# echo "  ${BASH_REMATCH[1]}"
-echo "${BASH_REMATCH[1]} "
+echo "$OS_ICON ${BASH_REMATCH[1]}"

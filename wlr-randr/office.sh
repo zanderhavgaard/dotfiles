e7c557c5e160702c4c27a77d77366b19f21181f8
zanderhavgaard <zander@havgaard.dk>
2025-03-18 16:19:51 +0100

    rearrange office monitors

--- wlr-randr/office.sh
+++ wlr-randr/office.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-wlr-randr \
-	--output eDP-1 --pos 2560,1440 --preferred --scale 1.0 \
-	--output DP-7 --pos 0,0 --preferred --scale 1.0 \
-	--output DP-8 --pos 2560,0 --preferred --scale 1.0
-
-# TODO: does not work ... dock issue?
 # wlr-randr \
-# 	--output eDP-1 --pos 2560,2160 --preferred --scale 1.0 \
-# 	--output DP-7 --pos 0,0 --mode 3840x2160@59.939999 --scale 1.5 \
-# 	--output DP-8 --pos 3840,0 --mode 3840x2160@59.939999 --scale 1.5
+#   --output eDP-1 --pos 2560,1440 --preferred --scale 1.0 \
+#   --output DP-7 --pos 0,0 --preferred --scale 1.0 \
+#   --output DP-8 --pos 2560,0 --preferred --scale 1.0
+
+wlr-randr \
+  --output DP-7 --pos 0,0 --preferred --scale 1.0 --transform 270 \
+  --output DP-8 --pos 1440,0 --preferred --scale 1.0 \
+  --output eDP-1 --pos 1440,1440 --preferred --scale 1.0

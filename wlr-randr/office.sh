2e994cffe43ae56ff9a32cc5f3bd4049267703f4
zanderhavgaard <zander@havgaard.dk>
2025-03-21 10:48:45 +0100

    update office.sh wlr-randr script

--- wlr-randr/office.sh
+++ wlr-randr/office.sh
@@ -9,3 +9,7 @@ wlr-randr \
   --output DP-7 --pos 0,0 --preferred --scale 1.0 --transform 270 \
   --output DP-8 --pos 1440,0 --preferred --scale 1.0 \
   --output eDP-1 --pos 1440,1440 --preferred --scale 1.0
+
+# restart swww-daemon
+killall swww-daemon
+riverctl spawn 'swww-daemon --format xrgb'

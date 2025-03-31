2b162619feba06f9833196cbc8819efa320dfd58
zanderhavgaard <zander@havgaard.dk>
2025-03-31 17:09:07 +0200

    rearrange office monitors

--- wlr-randr/office.sh
+++ wlr-randr/office.sh
@@ -5,10 +5,15 @@
 #   --output DP-7 --pos 0,0 --preferred --scale 1.0 \
 #   --output DP-8 --pos 2560,0 --preferred --scale 1.0
 
+# wlr-randr \
+#   --output DP-8 --pos 0,0 --preferred --scale 1.0 --transform 270 \
+#   --output DP-7 --pos 1440,0 --preferred --scale 1.0 \
+#   --output eDP-1 --pos 1440,1440 --preferred --scale 1.0
+
 wlr-randr \
-  --output DP-7 --pos 0,0 --preferred --scale 1.0 --transform 270 \
-  --output DP-8 --pos 1440,0 --preferred --scale 1.0 \
-  --output eDP-1 --pos 1440,1440 --preferred --scale 1.0
+  --output DP-7 --pos 0,0 --preferred --scale 1.0 \
+  --output DP-8 --pos 2560,0 --preferred --scale 1.0 --transform 90 \
+  --output eDP-1 --pos 640,1440 --preferred --scale 1.0
 
 # restart swww-daemon
 killall swww-daemon

eb30e165ad399946a081c574f1238b4d37be8657
zanderhavgaard <zander@havgaard.dk>
2025-05-18 23:51:10 +0200

    delete some unused scripts

--- scripts/install_tmux_plugin_manager.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# Installs the tmux plugin manager by cloning the git repo to the right directory
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
--- scripts/pia_status.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ $(piactl get connectionstate) = "Connected" ]]; then
-	ip=$(piactl get vpnip)
-	echo "vpn: $ip"
-else
-	ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
-	echo "public ip: $ip"
-fi

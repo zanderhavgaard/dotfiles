ba31cdc9232e99de8a062c9190c03757b5f8b471
zanderhavgaard <zander@havgaard.dk>
2025-05-18 23:59:12 +0200

    update swayidle script

--- scripts/swayidle.sh
+++ scripts/swayidle.sh
@@ -4,11 +4,5 @@
 killall swayidle >/dev/null 2>&1
 
 swayidle -w \
-	timeout 600 'swaylock -f -c 000000' \
-	before-sleep 'swaylock -f -c 000000'
-
-# swayidle -w \
-# 	timeout 300 'swaylock -f -c 000000' \
-# 	timeout 600 'swaymsg "output * power off"' \
-# 	resume 'swaymsg "output * power on"' \
-# 	before-sleep 'swaylock -f -c 000000'
+  timeout 600 'swaylock -f -c 000000' \
+  before-sleep 'swaylock -f -c 000000'

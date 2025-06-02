b95b7e309d9e631ab946481d08950d4025ad79b1
zanderhavgaard <zander@havgaard.dk>
2025-06-02 16:50:37 +0200

    comment out browser in default-apps script as it seems to break

--- scripts/default-apps.sh
+++ scripts/default-apps.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # Web browser
-xdg-settings set default-web-browser firefox.desktop
+# xdg-settings set default-web-browser firefox.desktop
 xdg-mime default firefox.desktop x-scheme-handler/http
 xdg-mime default firefox.desktop x-scheme-handler/https
 xdg-mime default firefox.desktop text/html

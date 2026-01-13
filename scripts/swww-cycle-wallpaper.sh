9dc6a4b85055c36322bd7caef61267e589146b70
zanderhavgaard <zander@havgaard.dk>
2026-01-13 23:30:29 +0100

    tweak swww-cycle wallpaper script

--- scripts/swww-cycle-wallpaper.sh
+++ scripts/swww-cycle-wallpaper.sh
@@ -2,4 +2,4 @@
 
 wallpaper_path="$HOME/wallpapers"
 wallpaper=$(ls ${wallpaper_path} | shuf -n 1)
-swww img --transition-type random "${wallpaper_path}/$wallpaper"
+swww img --transition-type any "${wallpaper_path}/$wallpaper"
